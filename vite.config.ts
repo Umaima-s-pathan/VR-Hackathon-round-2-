8762f2ceb11f8a338d20cad4bf9f7f5ac6057dd5
Umaima-s-pathan <105055412+Umaima-s-pathan@users.noreply.github.com>
2025-09-16 21:11:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/VR-180-Experience/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
